a828c1dde45242f36d7d2573301aaaaf64fdb90e
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-10-06 15:30:16 +0200

    init

---
+++ main.js
@@ -0,0 +1,58 @@
+import "./style.css"
+
+var canvas = document.getElementById("c")
+// Check for canvas support
+if (canvas.getContext) {
+	// Initialization
+	function init(image) {
+		canvas.width = image.naturalWidth
+		canvas.height = image.naturalHeight
+		ctx.drawImage(image, 0, 0)
+	}
+
+	const grayscale = function () {
+		var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+		var data = imageData.data
+		for (let i = 0; i < data.length; i += 4) {
+			let gray = (data[i] + data[i + 1] + data[i + 2]) / 3
+			data[i] = gray
+			data[i + 1] = gray
+			data[i + 2] = gray
+		}
+		ctx.putImageData(imageData, 0, 0)
+	}
+  
+	const grayscale = function () {
+		var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+		var data = imageData.data
+		for (let i = 0; i < data.length; i += 4) {
+			let gray = (data[i] + data[i + 1] + data[i + 2]) / 3
+			data[i] = gray
+			data[i + 1] = gray
+			data[i + 2] = gray
+		}
+		ctx.putImageData(imageData, 0, 0)
+	}
+	const invert = function () {
+		var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+		var data = imageData.data
+		for (let i = 0; i < data.length; i += 4) {
+			data[i] = 255 - data[i]
+			data[i + 1] = 255 - data[i + 1]
+			data[i + 2] = 255 - data[i + 2]
+		}
+		ctx.putImageData(imageData, 0, 0)
+	}
+	var ctx = canvas.getContext("2d")
+	var img = new Image()
+	img.src = "./us.png"
+	img.onload = function () {
+		init(this)
+	}
+	document.getElementById("grayscaleBtn").addEventListener("click", grayscale)
+	document.getElementById("invertBtn").addEventListener("click", invert)
+	document.getElementById("resetBtn").addEventListener("click", function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		ctx.drawImage(img, 0, 0)
+	})
+}
